710dd4caeeecb711281b759e2205b87e65171488
Keanu Kent Gargar <keanukent@ubx.com>
2025-08-10 14:55:33 +0800

    chore(logger): remove pino-pretty transport; use console in dev and file in prod

--- config/logger.ts
+++ config/logger.ts
@@ -16,7 +16,7 @@ const loggerConfig = defineConfig({
       level: env.get('LOG_LEVEL'),
       transport: {
         targets: targets()
-          .pushIf(!app.inProduction, targets.pretty())
+          .pushIf(!app.inProduction, targets.console())
           .pushIf(app.inProduction, targets.file({ destination: 1 }))
           .toArray(),
       },
